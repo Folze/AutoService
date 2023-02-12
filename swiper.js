45b093c90b2636dc8603e7fa5bdf9c64cbcb10dd
Folze <93938538+Folze@users.noreply.github.com>
2023-02-12 19:15:08 +0500

    Add files via upload

---
+++ swiper.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $('.swiper').slick({
+        adaptiveHeight: true,
+        speed: 1000,
+        prevArrow: '.btnLeft',
+        nextArrow: '.btnRight',
+    });
+})
+
+
+
+$(document).ready(function () {
+    $('.swiperComment').slick({
+        adaptiveHeight: true,
+        speed: 1000,
+        prevArrow: '.btnArrowComments',
+        nextArrow: '.btnArrowComments2',
+    });
+})
